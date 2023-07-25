c5517f264bcc1513a6d77291f7ba8a1f357266c3
willofD13 <leonidastsiakas13@gmail.com>
2023-07-25 16:23:29 +0300

    Write recursive methods for node children

--- main.rb
+++ main.rb
@@ -1,18 +1,22 @@
+
 class Node 
 
-    def initialize(value)
+    attr_accessor :value, :left_child, :right_child
+
+    def initialize(value,left_child = nil,right_child = nil)
         @value = value
-        @left = nil 
-        @right = nil
+        @left_child =  left_child
+        @right_child = right_child
     end 
 
 end
 
 class Tree  
+    attr_accessor :array, :root_node
 
     def initialize(array) 
         @array = array
-        @root = build_tree(array)
+        @root_node = build_tree(array)
     end
 
 
@@ -27,9 +31,8 @@ class Tree
         middle = (start + last)/2 #find the middle element
 
         root_node = Node.new(array[middle])
-
-        left_child = build_tree(array.slice(0..middle - 1))
-        right_child = build_tree(array.slice((middle + 1)..last))
+        root_node.left_child = build_tree(array[0...middle])
+        root_node.right_child = build_tree(array[(middle + 1)..last])
 
         return root_node
     end
@@ -37,6 +40,8 @@ class Tree
     def prepare_array(array)
         array.sort!.uniq!
     end
+
+
 
 end
 
